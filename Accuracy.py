c29b5ec45629cee646761e7dfb698d25a1408d0b
Emanuel Sujeta <esujeta05@gmail.com>
2025-08-10 20:41:03 +0200

    add add accuracy

---
+++ Accuracy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Accuracy:
+    def __init__(self, classification_type="categorical"):
+        self.classification_type = classification_type
+
+    def forward(self, y_true, y_pred):
+        if self.classification_type == "categorical":
+            print(y_true)
+            if len(y_true.shape) == 2:
+                y_true = np.argmax(y_true, axis=1)
+                print(y_true + "****")
+            y_pred = np.argmax(y_pred, axis=1)
+            print(y_pred)
+            return np.mean(y_true == y_pred)
+        elif self.classification_type == "binary":
+            y_pred = (y_pred > 0.5).astype(int)
+            return np.mean(y_true == y_pred)
+        else:
+            raise ValueError("Unknown classification_type")
