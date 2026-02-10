cf350d7e8fd79739cea65bbd026106ad9fa7deaa
Emanuel Sujeta <esujeta05@gmail.com>
2026-02-10 11:57:12 +0100

    Implement MNIST example, update optimizer and project dependencies

--- Optimizers.py
+++ Optimizers.py
@@ -20,15 +20,18 @@ class StochasticGradientDescent:
             if not hasattr(dense_layer, 'weight_momentums'):
                 dense_layer.weight_momentums = np.zeros_like(dense_layer.weights)
                 dense_layer.bias_momentums = np.zeros_like(dense_layer.biases)
+            
             weight_updates = self.momentum * dense_layer.weight_momentums - self.current_learning_rate * dense_layer.derivative_weights
             dense_layer.weight_momentums = weight_updates
+            
             bias_updates = self.momentum * dense_layer.bias_momentums - self.current_learning_rate * dense_layer.derivative_biases
             dense_layer.bias_momentums = bias_updates
+            
+            dense_layer.weights += weight_updates
+            dense_layer.biases += bias_updates
         else:
-            weight_updates = self.current_learning_rate * dense_layer.derivative_weights
-            bias_updates = self.current_learning_rate * dense_layer.derivative_biases
-        dense_layer.weights -= weight_updates
-        dense_layer.biases -= bias_updates
+            dense_layer.weights -= self.current_learning_rate * dense_layer.derivative_weights
+            dense_layer.biases -= self.current_learning_rate * dense_layer.derivative_biases
 
     def after_updating_params(self):
         self.iteration += 1
